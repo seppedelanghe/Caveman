24b91e47b99ed489439956cb7ffa2c08aefcd2b2
Seppe De Langhe <seppedelanghe17@gmail.com>
2022-02-07 21:17:24 +0100

    remove build script from git repo

--- build.ps1
+++
@@ -1,6 +0,0 @@
-python -m build
-pip wheel . -w wheels
-$wheels = Get-ChildItem -Path .\wheels\ -Name
-pip install .\wheels\$wheels --force-reinstall
-
-python -m twine upload --repository pypi dist/*
